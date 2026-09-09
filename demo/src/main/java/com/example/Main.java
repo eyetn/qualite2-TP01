5093229166d07d818acb9fcd0caa09928a8904e9
pepito-arc <pacoretvincent@gmail.com>
2026-09-09 15:48:13 +0200

    sub calculatrice

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,5 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculatrice");
+        System.out.println("Fonction substraction de la calculatrice");
     }
 }
