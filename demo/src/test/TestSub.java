f8a477ecc7960713ef7efbc2cb4e7ea3f05b3451
pepito-arc <pacoretvincent@gmail.com>
2026-09-09 13:21:30 +0200

    test

--- demo/src/test/TestSub.java
+++ demo/src/test/TestSub.java
@@ -2,7 +2,9 @@ import org.junit.*;
 import java.Sub.*;
 
 public class TestSub {
-    public static void testSub(){
-        Assert.assertEquals(3,Sub.sub(5, 2));
+
+    @Test
+    public void testSub(){
+        Assert.assertEquals("Echec test sub",3,Sub.sub(5, 2));
     }
 }
